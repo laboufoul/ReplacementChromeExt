17fef08cb6ff384eb5987d1eb79a19cef62ab120
Lolo Aboufoul <laboufou@uncc.edu>
2025-03-20 09:53:04 -0400

    removed extra comments

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-// Get all the elements that you care about into an array
-//let elements = Array.prototype.slice.call(document.querySelectorAll("p, h1, header"));
 function replaceTextInNode(node) {
   if (node.nodeType === 3) { // Text node
     node.nodeValue = node.nodeValue.replace(/amazon.com/1/gi, 'amazon.com/2');
@@ -8,5 +6,3 @@ function replaceTextInNode(node) {
   }
 }
 replaceTextInNode(document.body);
-
-//amazon.com/1 to amazon.com/2; take static url followed by an id and switch it out; 
