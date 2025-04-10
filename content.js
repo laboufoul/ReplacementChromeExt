41e5538221b44d32784e12a10449d26126910156
Lolo Aboufoul <laboufou@uncc.edu>
2025-04-10 14:47:22 -0400

    added ampersand to tag property in url

--- content.js
+++ content.js
@@ -12,7 +12,7 @@ let originalUrl = document.URL; //store product link into originalUrl variable
 
 function changeUrl(url){//url argument represents product link
   if (url.indexOf("https://www.amazon.com") > -1){ //checks to see if user is browsing a product on Amazon's website
-    let tag = "tag=apricot07-20";
+    let tag = "&tag=apricot07-20";
     let myNewUrl = url + tag;//adds Apricot storefront tag to product URL, adding it to our storefront
     chrome.tabs.update(undefined, { url: myNewUrl });//updating the url in the browser
   }
