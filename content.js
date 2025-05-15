f2918182295891d85cc4c43d5c982bd9c2d56980
boyle181 <aidanboyle851@gmail.com>
2025-05-15 13:37:19 -0500

    Added simplified affiliate replacer function

--- content.js
+++ content.js
@@ -8,30 +8,36 @@
 
 // replaceTextInNode(document.body);
 
-let originalUrl = document.URL; //store product link into originalUrl variable
+// let originalUrl = document.URL; //store product link into originalUrl variable
 
-function changeUrl(url){//url argument represents product link
-  if (url.indexOf("https://www.amazon.com") > -1){ //checks to see if user is browsing a product on Amazon's website
-    let tag = "&tag=apricot07-20";
-    let myNewUrl = url + tag;//adds Apricot storefront tag to product URL, adding it to our storefront
-    chrome.tabs.update(undefined, { url: myNewUrl });//updating the url in the browser
-  }
-}
+// function changeUrl(url){//url argument represents product link
+//   if (url.indexOf("https://www.amazon.com") > -1){ //checks to see if user is browsing a product on Amazon's website
+//     let tag = "&tag=apricot07-20";
+//     let myNewUrl = url + tag;//adds Apricot storefront tag to product URL, adding it to our storefront
+//     chrome.tabs.update(undefined, { url: myNewUrl });//updating the url in the browser
+//   }
+// }
 
 // Added by Aidan:
-// Redirects from a specific affiliate link to apricot
-// Example URL is http://www.amazon.com/dp/ASIN/ref=nosim?tag=YOURASSOCIATEID
-// function replaceAffiliateLink() {
-//   const apricotAffiliateLink = "https://www.amazon.com?&linkCode=ll2&tag=apricot07-20&linkId=fcce49cc2af4a1fff274a6a60cb2d083&language=en_US&ref_=as_li_ss_tl";
-//   const otherAffiliateLink = "tag=YOURASSOCIATEID";
-//   const url = new URL('', window.location.href);
-//   if (url.hostname === 'www.amazon.com') {
-//     if (window.location.href.includes(otherAffiliateLink)) {
-//       window.location.href = apricotAffiliateLink;
-//     }
-//   } 
-// } 
+// Redirects from any amazon product to apricot affiliate link with that amazon product
+function replaceURL() {
+  const currentURL = window.location.href;
+  const url = new URL('', window.location.href);
 
+  if (url.hostname === 'www.amazon.com') {
+    if (!currentURL.includes('tag=apricot07-20')) {
+      const dpPath = currentURL.match(/\/dp\/[A-Z0-9]{10}/)?.[0];
+      if (dpPath === undefined) {
+        window.location.href = 'https://www.amazon.com'+"?th=1&linkCode=ll2&tag=apricot07-20";
+      } else {
+        window.location.href = 'https://www.amazon.com'+dpPath+"?th=1&linkCode=ll2&tag=apricot07-20";
+      }    
+    } else {
+      console.log("in apricot affiliate session")
+    }
+  } 
 
-changeUrl(originalUrl); 
-// replaceAffiliateLink();
+}
+
+// changeUrl(originalUrl); 
+replaceURL();
